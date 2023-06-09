212b40a98f33ea6304a718fa174674faead73470
mtan9865 <merttanatmis@gmail.com>
2023-06-08 20:25:15 -0400

    Step 7

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function getTasksFromLocalStorage() {
 }
 
 function updateTasksInLocalStorage() {
-    JSON.parse(localStorage.setItem("taskList", JSON.stringify(taskList))) || []
+    localStorage.setItem("taskList", JSON.stringify(taskList))
 }
 
 function displayList() {
@@ -46,4 +46,38 @@ function createTask(taskText) {
 function deleteTask(index) {
     taskList.splice(index, 1)
     updateTasksInLocalStorage()
+}
+
+function createTaskElement(taskObj) {
+    const taskItem = document.createElement("li");
+    taskItem.classList.add("taskItem");
+  
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("checkbox");
+    checkbox.checked = taskObj.completed;
+    checkbox.addEventListener("change", function () {
+        taskObj.completed = checkbox.checked;
+        taskTextElement.classList.toggle("completed", taskObj.completed);
+        updateTasksInLocalStorage();
+    });
+  
+    const taskTextElement = document.createElement("span");
+    taskTextElement.classList.add("taskText");
+    taskTextElement.textContent = taskObj.text;
+    taskTextElement.classList.toggle("completed", taskObj.completed);
+  
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("removeButton");
+    removeButton.textContent = "X";
+    removeButton.addEventListener("click", function () {
+        deleteTask(taskObj);
+        renderTasks();
+    });
+  
+    taskItem.appendChild(checkbox);
+    taskItem.appendChild(taskTextElement);
+    taskItem.appendChild(removeButton);
+  
+    return taskItem;
 }
